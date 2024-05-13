1db05c94349a83f2995234f89024df137784cbb9
Muhammad Ali Zufan <muhammadalizufan@gmail.com>
2024-05-14 03:46:46 +0700

    feat: update response get game, get product list, sql generator and logger

--- main.js
+++ main.js
@@ -7,30 +7,56 @@ const bodyParser = require('body-parser')
 const port = 3000
 const data = require('./data/products.json');
 
+const joinSpace = (...str) => {
+    return str.join(" ")
+}
+
 // t_bill_product sql generators
 const billProductSQL = data.map(v => {
-    return "INSERT INTO `t_bill_product` (`transaction_type_code`, `secondary_category_code`, `aggregator`, `biller`, `mobile_prefix_pattern`, `product_id`, `product_logo_url`, `product_name`, `admin_fee`, `commission_fee`, `channel_fee`, `service_fee`, `is_point_enabled`, `is_valid`, `created_by`, `created_time`, `updated_by`, `updated_time` ) " +
-    "VALUES ( 'Prepaid', '0402000000', '"+v.aggregator+"', '"+v.biller+"', '-', '"+v.productId+"', 'https://placehold.co/500x500.png', '"+v.productName+"', '"+v.adminFee+"', '"+v.commisionFee+"', '0', '0', 'N', 'Y', 'system', CURRENT_DATE(), 'system', CURRENT_DATE() );"
+    return joinSpace(
+        "INSERT INTO `t_bill_product` (`transaction_type_code`, `secondary_category_code`, `aggregator`, `biller`, `mobile_prefix_pattern`, `product_id`, `product_logo_url`, `product_name`, `admin_fee`, `commission_fee`, `channel_fee`, `service_fee`, `is_point_enabled`, `is_valid`, `created_by`, `created_time`, `updated_by`, `updated_time` )",
+        `VALUES ( 'Prepaid', '0402000000', '${v.aggregator}', '${v.biller}', '-', '${v.productId}', 'https://placehold.co/500x500.png', '${v.productName}', '${v.adminFee}', '${v.commisionFee}', '0', '0', 'N', 'Y', 'system', CURRENT_DATE(), 'system', CURRENT_DATE() );`
+    )
 })
 
-const filenameProduct = "insert record - bill product - game voucher.sql"
+const filenameProduct = "insert-bill-product-game-voucher.sql"
 fs.writeFile(`./data/${filenameProduct}`, billProductSQL.join("\n"), err => {
-    if (err) throw err;
-    console.log(`Write and Replace File '${filenameProduct}' SQL!`);
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`> Replace and Write File '${filenameProduct}'!\n`);
 })
 
 // t_bill_product_detail sql generators
 const billProductDetailSQL = data.map(v => {
-    return "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`) VALUES ('"+v.productId+"', 'GAME_VOUCHER_AGGREGATOR_CODE', '"+v.aggregatorCode+"');\n"+
-    "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`) VALUES ('"+v.productId+"', 'GAME_VOUCHER_DEFAULT_ADMIN_FEE', '"+v.adminFee.toFixed(2)+"');\n"+
-    "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`) VALUES ('"+v.productId+"', 'GAME_VOUCHER_PRODUCT_CODE', '"+v.productCode+"');\n"+
-    "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`) VALUES ('"+v.productId+"', 'GAME_VOUCHER_SELL_PRICE', '"+v.sellPrice.toFixed(2)+"');"
+    return [
+        joinSpace(
+            "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`)",
+            `VALUES ('${v.productId}', 'GAME_VOUCHER_AGGREGATOR_CODE', '${v.aggregatorCode}');\n`,
+        ),
+        joinSpace(
+            "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`)",
+            `VALUES ('${v.productId}', 'GAME_VOUCHER_PRODUCT_CODE', '${v.productCode}');\n`,
+        ),
+        joinSpace(
+            "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`)",
+            `VALUES ('${v.productId}', 'GAME_VOUCHER_SELL_PRICE', '${v.sellPrice.toFixed(2)}');`
+        ),
+        joinSpace(
+            "INSERT INTO `t_bill_product_detail` (`bill_product_id`, `type`, `value`)",
+            `VALUES ('${v.productId}', 'GAME_VOUCHER_DEFAULT_ADMIN_FEE', '${v.adminFee.toFixed(2)}');\n`,
+        )
+    ].join("")
 })
 
-const filenameProductDetail = "insert record - bill product detail - game voucher.sql"
+const filenameProductDetail = "insert-bill-product-detail-game-voucher.sql"
 fs.writeFile(`./data/${filenameProductDetail}`, billProductDetailSQL.join("\n\n"), err => {
-    if (err) throw err;
-    console.log(`Write and Replace File '${filenameProductDetail}' SQL!`);
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`> Replace and Write File '${filenameProductDetail}'!\n`);
 })
 
 const product = data
@@ -39,7 +65,9 @@ const product = data
             productId: v.productId,
             productName: v.productName,
             productCode: v.productCode,
-            gameName: v.biller,
+            aggregator: v.aggregator,
+            aggregatorCode: v.aggregatorCode,
+            biller: v.biller,
             productLogoUrl: 'https://placehold.co/500x500.png',
             billAmount: v.sellPrice.toFixed(2),
             retailPriceAmount: v.retailPrice.toFixed(2),
@@ -65,11 +93,11 @@ const operator = data
     .map(v => ({
         aggregatorCode: v.aggregatorCode,
         productCode: v.productCode,
-        gameName: v.biller,
+        biller: v.biller,
         imageUrl: 'https://placehold.co/500x500.png'
     }))
     .sort((a, b) => {
-        const [ad, bd] = [a.gameName.toLowerCase(), b.gameName.toLowerCase()]
+        const [ad, bd] = [a.biller.toLowerCase(), b.biller.toLowerCase()]
         if (ad < bd) return -1;
         if (ad > bd) return 1;
         return 0;
@@ -96,7 +124,7 @@ const app = express()
 // middlewares
 app.use(bodyParser.json())
 app.use(cors())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(morgan(':method | :url | :status | :res[content-length] - :response-time ms'))
 
 // endpoints
 app.post('/api/v1/bill-payment/game-voucher/get-product-list', (req, res) => {
